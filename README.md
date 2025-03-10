64bb2b04a4abc9a5a56e658e2f82700a076a6c37
gaetan <gmusinde25@gmail.com>
2025-03-10 12:22:52 +0100

    second commit

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Rust_Postgres_axum_sqlx_dioxus
-# Rust_backend
-# Rust_backend
-# Axum_Sqlx_Dioxus_backend
+
--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
-echo "# Rust_Postgres_axum_sqlx_dioxus" >> README.md
+echo "# Axum_Sqlx_Dioxus_backend" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Gaetan2023/Rust_Postgres_axum_sqlx_dioxus.git
+git remote add origin https://github.com/Gaetan2023/Axum_Sqlx_Dioxus_backend.git
 git push -u origin main
